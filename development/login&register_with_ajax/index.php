4c8450859b792ab8cfe621c56d8400cb94d0850c
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 12:08:56 +0530

    Update index.php

--- development/login&register_with_ajax/index.php
+++ development/login&register_with_ajax/index.php
@@ -25,13 +25,13 @@
 	<form id="login_form" name="form1" method="post" >
 
 		<div class="form-floating mb-3">
-		<input type="email" class="form-control" id="floatingInput" placeholder="Email" name="email" aria-describedby="emailHelp">
-		<label for="floatingInput" class="form-label">Email address</label>
-        </div>
+		<label for="pwd" class="form-label">Email address</label>
+			<input type="email" class="form-control" id="email_log" placeholder="Email" name="email" aria-describedby="emailHelp">
+		</div>
 
-		<div class="form-floating mb-3">
-			<label for="floatingPassword">Password:</label>
-			<input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
+		<div class="form-floating  mb-3">
+			<label for="pwd">Password:</label>
+			<input type="password" class="form-control" id="password_log" placeholder="Password" name="password">
 		</div>
         <div class="d-grid gap-2">
         <input type="button" name="save" class="btn btn-success " value="Login" id="butlogin">
