467e84789076054697217b9f550b4a650db27e47
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 12:12:10 +0530

    Update index.php

--- development/login&register_with_ajax/index.php
+++ development/login&register_with_ajax/index.php
@@ -25,12 +25,12 @@
 	<form id="login_form" name="form1" method="post" >
 
 		<div class="form-floating mb-3">
-		    <label for="pwd" class="form-label">Email address</label>
+		    <label for="email_log" class="form-label">Email address</label>
 			<input type="email" class="form-control" id="email_log" placeholder="Email" name="email" aria-describedby="emailHelp">
 		</div>
 
 		<div class="form-floating  mb-3">
-			<label for="pwd">Password</label>
+			<label for="password_log">Password</label>
 			<input type="password" class="form-control" id="password_log" placeholder="Password" name="password">
 		</div>
         <div class="d-grid gap-2">
@@ -43,19 +43,19 @@
 
 <form id="register_form" name="form1" method="post" style="display:none;">
 		<div class="form-floating mb-3">
-			<label for="email">Name:</label>
+			<label for="name">Name:</label>
 			<input type="text" class="form-control" id="name" placeholder="Name" name="name">
 		</div>
 		<div class="form-floating mb-3">
-			<label for="pwd">Email:</label>
+			<label for="email">Email:</label>
 			<input type="email" class="form-control" id="email" placeholder="Email" name="email">
 		</div>
 		<div class="form-floating mb-3">
-			<label for="pwd">Phone:</label>
+			<label for="phone">Phone:</label>
 			<input type="text" class="form-control" id="phone" placeholder="Phone" name="phone">
 		</div>
 		<div class="form-floating mb-3" >
-			<label for="pwd">City:</label>
+			<label for="city">City:</label>
 			<select name="city" id="city" class="form-control">
 				<option value="">Select</option>
 				<option value="Delhi">Delhi</option>
