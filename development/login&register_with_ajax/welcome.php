d5f932289b417175ac6e6a34ce5b4e9133992d8c
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 11:22:22 +0530

    Update welcome.php

--- development/login&register_with_ajax/welcome.php
+++ development/login&register_with_ajax/welcome.php
@@ -7,3 +7,15 @@ if(!isset($_SESSION['email']) || empty($_SESSION['email'])) {
     echo 'Welcome ' . $_SESSION['email'];
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <a href="logout.php">logout</a>
+</body>
+</html>
