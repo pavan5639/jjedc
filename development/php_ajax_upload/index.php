5fd8c794064b59be12c76e712706fcfe14eb62bf
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 12:45:02 +0530

    Update index.php

--- development/php_ajax_upload/index.php
+++ development/php_ajax_upload/index.php
@@ -6,7 +6,7 @@
 
    <!-- jquery links -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-   
+
  </head>
  <body>
   <div class="container">
@@ -33,9 +33,6 @@
     </div>
    </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-
   <script>
 $(document).ready(function(){
  $('#uploadImage').submit(function(event){
@@ -68,5 +65,9 @@ $(document).ready(function(){
  });
 });
 </script>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
  </body>
 </html>
