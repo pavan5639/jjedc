0f79c41d035ec61ac90bd82dcb66a5da40a96863
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 11:43:29 +0530

    Update index.php

--- development/login&register_with_ajax/index.php
+++ development/login&register_with_ajax/index.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<title>Insert data in MySQL database using Ajax</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
@@ -146,6 +147,10 @@ $(document).ready(function() {
 	});
 });
 </script>
+
+ <!-- Option 1: Bootstrap Bundle with Popper -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
 </body>
 </html>    
      
