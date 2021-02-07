99bb3ba93e59231706f93d985934f3aca0f3358b
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 11:06:57 +0530

    a

---
+++ development/login&register_with_ajax/db/db.php
@@ -0,0 +1,8 @@
+<?php
+	$servername = "localhost";
+	$username = "u815129216_dev";
+	$password = "Rats1851";
+	$db="u815129216_dev";
+	/*Create connection*/
+	$conn = mysqli_connect($servername, $username, $password,$db);
+?>
