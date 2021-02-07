d6643fe24087ffa8111db9bc699d4e72e98e5491
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 11:30:15 +0530

    Update index.php

--- development/login&register_with_ajax/index.php
+++ development/login&register_with_ajax/index.php
@@ -14,10 +14,24 @@
 	  <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
 	</div>
 	<button type="button" class="btn btn-success btn-sm" id="register">Register</button> <button type="button" class="btn btn-success btn-sm" id="login">Login</button>
-	
+
+<!-- Login Form -->
+
+	<form id="login_form" name="form1" method="post" >
+		
+		<div class="form-group">
+			<label for="pwd">Email:</label>
+			<input type="email" class="form-control" id="email_log" placeholder="Email" name="email">
+		</div>
+		<div class="form-group">
+			<label for="pwd">Password:</label>
+			<input type="password" class="form-control" id="password_log" placeholder="Password" name="password">
+		</div>
+		<input type="button" name="save" class="btn btn-primary" value="Login" id="butlogin">
+	</form>
 <!-- Register Form -->
 
-	<form id="register_form" name="form1" method="post" style="display:none;">
+<form id="register_form" name="form1" method="post" style="display:none;">
 		<div class="form-group">
 			<label for="email">Name:</label>
 			<input type="text" class="form-control" id="name" placeholder="Name" name="name">
@@ -45,23 +59,6 @@
 		</div>
 		<input type="button" name="save" class="btn btn-primary" value="Register" id="butsave">
 	</form>
-
-
-<!-- Login Form -->
-
-	<form id="login_form" name="form1" method="post" >
-		
-		<div class="form-group">
-			<label for="pwd">Email:</label>
-			<input type="email" class="form-control" id="email_log" placeholder="Email" name="email">
-		</div>
-		<div class="form-group">
-			<label for="pwd">Password:</label>
-			<input type="password" class="form-control" id="password_log" placeholder="Password" name="password">
-		</div>
-		<input type="button" name="save" class="btn btn-primary" value="Login" id="butlogin">
-	</form>
-
 
     
 </div>
