be78d86154dd9695d1c2eba57a87a42c110b8bcd
pavan5639 <pavan5639kumar@gmail.com>
2021-02-07 12:17:58 +0530

    Update index.php

--- development/login&register_with_ajax/index.php
+++ development/login&register_with_ajax/index.php
@@ -11,7 +11,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-<div style="margin: auto;width: 40%;margin-top:250px;">
+<div style="margin: auto;width: 40%;margin-top:170px;">
 
 	<div class="alert alert-success alert-dismissible" id="success" style="display:none;">
 	  <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
