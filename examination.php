a7226bf777397303913fa9b6ee59d290fb414a1c
ajaykumar <kumarsajay473@gmail.com>
2021-02-19 15:44:48 +0530

    Update examination.php

--- examination.php
+++ examination.php
@@ -203,9 +203,9 @@ $msg = "";
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
           <div class="col-md-6 ftco-animate">
-            <h1 class="mb-4">Education Needs Complete Solution</h1>
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-            <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p>
+            <h1 class="mb-4">Examination</h1>
+            <!-- <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p> -->
+            <!-- <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p> -->
           </div>
         </div>
         </div>
